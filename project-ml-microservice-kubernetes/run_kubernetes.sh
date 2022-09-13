08a10237e309e9b331b3119e430d01ec066e8e56
EC2 Default User <ec2-user@ip-172-31-8-218.ec2.internal>
2022-09-13 15:29:41 +0000

    adding new files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="fahmykhalilm/ml_project"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment ml-app --image=$dockerpath:ml-app --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get deployment
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment ml-app --port=80 --target-port=80
+kubectl port-forward deployment/ml-app 8000:80
