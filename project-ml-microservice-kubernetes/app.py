c7ebc02deeee5da9d4defa4ab921c934f433c8db
EC2 Default User <ec2-user@ip-172-31-3-240.ec2.internal>
2022-09-13 14:17:00 +0000

    adding new files

--- project-ml-microservice-kubernetes/app.py
+++ project-ml-microservice-kubernetes/app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -55,14 +55,18 @@ def predict():
     
     # Logging the input payload
     json_payload = request.json
+    print(json_payload)
     LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
+    print(inference_payload)
     LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
     # scale the input
     scaled_payload = scale(inference_payload)
     # get an output prediction from the pretrained model, clf
+    print(scaled_payload)
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f"output prediction: {prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="fahmykhalilm/ml_project"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag ml-app:latest $dockerpath:ml-app
+cat .my_password.txt | docker login --username fahmykhalilm --password-stdin
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:ml-app
